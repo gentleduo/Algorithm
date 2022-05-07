e02c37d0af7e98ccac72d2b144a22f11e8e66fd9
gentleduo <gentleduo@163.com>
2022-05-07 19:47:36 +0800

    快速排序

--- src/org/duo/master/chapter005/Code02_PartitionAndQuickSort.java
+++ src/org/duo/master/chapter005/Code02_PartitionAndQuickSort.java
@@ -1,198 +1,242 @@
 package org.duo.master.chapter005;
 
+import java.util.Stack;
+
 public class Code02_PartitionAndQuickSort {
 
-	public static void swap(int[] arr, int i, int j) {
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
-	}
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 
-	// arr[L..R]上，以arr[R]位置的数做划分值
-	// 小于等于arr[R]的放左边，大于arr[R]的放右边，并且arr[R]放在小于等于区域的最右边
-	public static int partition(int[] arr, int L, int R) {
-		if (L > R) {
-			return -1;
-		}
-		if (L == R) {
-			return L;
-		}
-		int lessEqual = L - 1;
-		int index = L;
-		while (index < R) {
-			if (arr[index] <= arr[R]) {
-				swap(arr, index, ++lessEqual);
-			}
-			index++;
-		}
-		swap(arr, ++lessEqual, R);
-		return lessEqual;
-	}
+    // arr[L..R]上，以arr[R]位置的数做划分值
+    // 小于等于arr[R]的放左边，大于arr[R]的放右边，并且arr[R]放在小于等于区域的最右边
+    public static int partition(int[] arr, int L, int R) {
+        if (L > R) {
+            return -1;
+        }
+        if (L == R) {
+            return L;
+        }
+        int lessEqual = L - 1;
+        int index = L;
+        while (index < R) {
+            if (arr[index] <= arr[R]) {
+                swap(arr, index, ++lessEqual);
+            }
+            index++;
+        }
+        swap(arr, ++lessEqual, R);
+        return lessEqual;
+    }
 
-	// arr[L...R] 玩荷兰国旗问题的划分，以arr[R]做划分值
-	// <arr[R] ==arr[R] > arr[R]
-	public static int[] netherlandsFlag(int[] arr, int L, int R) {
-		if (L > R) { // L...R L>R
-			return new int[] { -1, -1 };
-		}
-		if (L == R) {
-			return new int[] { L, R };
-		}
-		int less = L - 1; // < 区 右边界
-		int more = R; // > 区 左边界
-		int index = L;
-		while (index < more) { // 当前位置，不能和 >区的左边界撞上
-			if (arr[index] == arr[R]) {
-				index++;
-			} else if (arr[index] < arr[R]) {
+    // arr[L...R] 玩荷兰国旗问题的划分，以arr[R]做划分值
+    // <arr[R] ==arr[R] > arr[R]
+    public static int[] netherlandsFlag(int[] arr, int L, int R) {
+        if (L > R) { // L...R L>R
+            return new int[]{-1, -1};
+        }
+        if (L == R) {
+            return new int[]{L, R};
+        }
+        int less = L - 1; // < 区 右边界
+        int more = R; // > 区 左边界
+        int index = L;
+        while (index < more) { // 当前位置，不能和 >区的左边界撞上
+            if (arr[index] == arr[R]) {
+                index++;
+            } else if (arr[index] < arr[R]) {
 //				swap(arr, less + 1, index);
 //				less++;
 //				index++;						
-				swap(arr, index++, ++less);
-			} else { // >
-				swap(arr, index, --more);
-			}
-		}
-		swap(arr, more, R); // <[R]   =[R]   >[R]
-		return new int[] { less + 1, more };
-	}
+                swap(arr, index++, ++less);
+            } else { // >
+                swap(arr, index, --more);
+            }
+        }
+        swap(arr, more, R); // <[R]   =[R]   >[R]
+        return new int[]{less + 1, more};
+    }
 
-	/**
-	 * 快排1和快排2的时间复杂都为O(N^2)
-	 * 最差的情况是为类似这样的数组排序：[1,2,3,4,5,6,7]，并且以最后一个数作为比较数
-	 * 第一次partition的时候会比较6个数，第二次partition比较5个数，因此整个算法的常数操作和为一个等差数列，
-	 * @param arr
-	 */
-	public static void quickSort1(int[] arr) {
-		if (arr == null || arr.length < 2) {
-			return;
-		}
-		process1(arr, 0, arr.length - 1);
-	}
+    /**
+     * 快排1和快排2的时间复杂都为O(N^2)
+     * 最差的情况是为类似这样的数组排序：[1,2,3,4,5,6,7]，并且以最后一个数作为比较数
+     * 第一次partition的时候会比较6个数，第二次partition比较5个数，因此整个算法的常数操作和为一个等差数列，
+     *
+     * @param arr
+     */
+    public static void quickSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process1(arr, 0, arr.length - 1);
+    }
 
-	public static void process1(int[] arr, int L, int R) {
-		if (L >= R) {
-			return;
-		}
-		// L..R partition arr[R] [ <=arr[R] arr[R] >arr[R] ]
-		int M = partition(arr, L, R);
-		process1(arr, L, M - 1);
-		process1(arr, M + 1, R);
-	}
-	
-	public static void quickSort2(int[] arr) {
-		if (arr == null || arr.length < 2) {
-			return;
-		}
-		process2(arr, 0, arr.length - 1);
-	}
+    public static void process1(int[] arr, int L, int R) {
+        if (L >= R) {
+            return;
+        }
+        // L..R partition arr[R] [ <=arr[R] arr[R] >arr[R] ]
+        int M = partition(arr, L, R);
+        process1(arr, L, M - 1);
+        process1(arr, M + 1, R);
+    }
 
-	// arr[L...R] 排有序，快排2.0方式
-	public static void process2(int[] arr, int L, int R) {
-		if (L >= R) {
-			return;
-		}
-		// [ equalArea[0]  ,  equalArea[0]]
-		int[] equalArea = netherlandsFlag(arr, L, R);
-		process2(arr, L, equalArea[0] - 1);
-		process2(arr, equalArea[1] + 1, R);
-	}
+    public static void quickSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process2(arr, 0, arr.length - 1);
+    }
 
-	/**
-	 * 快排3的时间复杂度能做到O(N*logN)，原因如下：
-	 * 1.通过快排为类似这样的数组：[1,2,3,7,5,6,4]进行排序时，时间复杂度最低，因为第一次partition的时间复杂度为O(N)
-	 * 然后2个子集分别为[1,2,3]、[5,6,7]，即：N/2；那么：T(N) = 2  * T(N / 2) + O (N^1)；根据Master公式可以得到时间复杂度为：O(N*logN)；
-	 * 2.通过快排为类似这样的数组：[1,2,3,4,5,6,7]进行排序时，时间复杂度最高：O(N^2)；
-	 * 通过数学证明，通过随机数的方式选择比较数，可以使选择到最佳比较数概率趋近100%，因此快排3的时间复杂度为：O(N*logN)
-	 * @param arr
-	 */
-	public static void quickSort3(int[] arr) {
-		if (arr == null || arr.length < 2) {
-			return;
-		}
-		process3(arr, 0, arr.length - 1);
-	}
+    // arr[L...R] 排有序，快排2.0方式
+    public static void process2(int[] arr, int L, int R) {
+        if (L >= R) {
+            return;
+        }
+        // [ equalArea[0]  ,  equalArea[0]]
+        int[] equalArea = netherlandsFlag(arr, L, R);
+        process2(arr, L, equalArea[0] - 1);
+        process2(arr, equalArea[1] + 1, R);
+    }
 
-	public static void process3(int[] arr, int L, int R) {
-		if (L >= R) {
-			return;
-		}
-		// 随机选取数组中某个下标的值和最右边的值进行交换
-		swap(arr, L + (int) (Math.random() * (R - L + 1)), R);
-		int[] equalArea = netherlandsFlag(arr, L, R);
-		process3(arr, L, equalArea[0] - 1);
-		process3(arr, equalArea[1] + 1, R);
-	}
+    /**
+     * 快排3的时间复杂度能做到O(N*logN)，原因如下：
+     * 1.通过快排为类似这样的数组：[1,2,3,7,5,6,4]进行排序时，时间复杂度最低，因为第一次partition的时间复杂度为O(N)
+     * 然后2个子集分别为[1,2,3]、[5,6,7]，即：N/2；那么：T(N) = 2  * T(N / 2) + O (N^1)；根据Master公式可以得到时间复杂度为：O(N*logN)；
+     * 2.通过快排为类似这样的数组：[1,2,3,4,5,6,7]进行排序时，时间复杂度最高：O(N^2)；
+     * 通过数学证明，通过随机数的方式选择比较数，可以使选择到最佳比较数概率趋近100%，因此快排3的时间复杂度为：O(N*logN)
+     *
+     * @param arr
+     */
+    public static void quickSort3(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process3(arr, 0, arr.length - 1);
+    }
 
-	// for test
-	public static int[] generateRandomArray(int maxSize, int maxValue) {
-		int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
-		}
-		return arr;
-	}
+    public static void process3(int[] arr, int L, int R) {
+        if (L >= R) {
+            return;
+        }
+        // 随机选取数组中某个下标的值和最右边的值进行交换
+        swap(arr, L + (int) (Math.random() * (R - L + 1)), R);
+        int[] equalArea = netherlandsFlag(arr, L, R);
+        process3(arr, L, equalArea[0] - 1);
+        process3(arr, equalArea[1] + 1, R);
+    }
 
-	// for test
-	public static int[] copyArray(int[] arr) {
-		if (arr == null) {
-			return null;
-		}
-		int[] res = new int[arr.length];
-		for (int i = 0; i < arr.length; i++) {
-			res[i] = arr[i];
-		}
-		return res;
-	}
+    // 快排非递归版本需要的辅助类
+    // 要处理的是什么范围上的排序
+    public static class Op {
+        public int l;
+        public int r;
 
-	// for test
-	public static boolean isEqual(int[] arr1, int[] arr2) {
-		if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
-			return false;
-		}
-		if (arr1 == null && arr2 == null) {
-			return true;
-		}
-		if (arr1.length != arr2.length) {
-			return false;
-		}
-		for (int i = 0; i < arr1.length; i++) {
-			if (arr1[i] != arr2[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
+        public Op(int left, int right) {
+            l = left;
+            r = right;
+        }
+    }
 
-	// for test
-	public static void printArray(int[] arr) {
-		if (arr == null) {
-			return;
-		}
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
-	}
+    // 快排3.0 非递归版本
+    public static void quickSort4(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        swap(arr, (int) (Math.random() * N), N - 1);
+        int[] equalArea = netherlandsFlag(arr, 0, N - 1);
+        int el = equalArea[0];
+        int er = equalArea[1];
+        Stack<Op> stack = new Stack<>();
+        stack.push(new Op(0, el - 1));
+        stack.push(new Op(er + 1, N - 1));
+        while (!stack.isEmpty()) {
+            Op op = stack.pop(); // op.l  ... op.r
+            if (op.l < op.r) {
+                swap(arr, op.l + (int) (Math.random() * (op.r - op.l + 1)), op.r);
+                equalArea = netherlandsFlag(arr, op.l, op.r);
+                el = equalArea[0];
+                er = equalArea[1];
+                stack.push(new Op(op.l, el - 1));
+                stack.push(new Op(er + 1, op.r));
+            }
+        }
+    }
 
-	// for test
-	public static void main(String[] args) {
-		int testTime = 500000;
-		int maxSize = 100;
-		int maxValue = 100;
-		boolean succeed = true;
-		for (int i = 0; i < testTime; i++) {
-			int[] arr1 = generateRandomArray(maxSize, maxValue);
-			int[] arr2 = copyArray(arr1);
-			int[] arr3 = copyArray(arr1);
-			quickSort1(arr1);
-			quickSort2(arr2);
-			quickSort3(arr3);
-			if (!isEqual(arr1, arr2) || !isEqual(arr2, arr3)) {
-				succeed = false;
-				break;
-			}
-		}
-		System.out.println(succeed ? "Nice!" : "Oops!");
-	}
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            int[] arr3 = copyArray(arr1);
+            int[] arr4 = copyArray(arr1);
+            quickSort1(arr1);
+            quickSort2(arr2);
+            quickSort3(arr3);
+            quickSort4(arr4);
+            if (!isEqual(arr1, arr2) || !isEqual(arr2, arr3) || !isEqual(arr3, arr4)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Oops!");
+    }
 }
